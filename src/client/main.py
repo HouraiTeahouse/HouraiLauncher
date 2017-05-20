db7399b210a49343e0294dee9ef6987258ffd366
james7132 <james.siqi.liu@gmail.com>
2017-05-20 13:03:14 -0700

    Add loading config

--- src/client/main.py
+++ src/client/main.py
@@ -1,30 +1,56 @@
+import os
 import sys
+import json
+from collections import OrderedDict
+from common.util import namedtuple_from_mapping
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 
 WIDTH = 640
 HEIGHT = 480
 
+CONFIG_FILE = 'config.json'
+
+# Get the base directory the executable is found in
+# When running from a python interpretter, it will use the current working
+# directory.
+# sys.frozen is an attribute injected by pyinstaller at runtime
+if getattr(sys, 'frozen', False):
+    BASE_DIR = os.path.dirname(os.path.abspath(sys.executable))
+else:
+    BASE_DIR = os.getcwd()
+
+
+# Load Config
+with open(os.path.join(BASE_DIR, CONFIG_FILE)) as config_file:
+    # Using OrderedDict to preserve JSON ordering of dictionaries
+    CONFIG = namedtuple_from_mapping(
+        json.load(config_file, object_pairs_hook=OrderedDict))
+
+
 class MainWindow(QWidget):
 
-    def __init__(self):
+    def __init__(self, config):
         super().__init__()
+        self.config = config
         self.init_ui()
 
     def init_ui(self):
-        self.resize(WIDTH, HEIGHT)
-        self.setWindowTitle('Hourai Launcher')
+        self.setFixedSize(WIDTH, HEIGHT)
+        self.setWindowTitle(self.config.project)
         # self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
-        btn = QPushButton('Launch Game', self)
-        btn.clicked.connect(self.launch_game)
+        self.launch_game_btn = QPushButton('Launch Game', self)
+        self.launch_game_btn.clicked.connect(self.launch_game)
+        for branch, name in self.config.branches.items():
+            print (branch, name)
 
     def launch_game(self):
         print ('Launching game...')
-        pass
+        self.launch_game_btn.setEnabled(False)
+
 
 def main():
     app = QApplication(sys.argv)
-    main_window = MainWindow()
-    main_window.center()
+    main_window = MainWindow(CONFIG)
     main_window.show()
     sys.exit(app.exec_())
 
