fadde4c4235bb848df31b298fe1cc0fa32064ba1
james7132 <contact@jamessliu.com>
2017-05-20 18:42:03 -0700

    PEP8

--- test/test_util.py
+++ test/test_util.py
@@ -1,10 +1,12 @@
 from unittest import TestCase, main
 from util import namedtuple_from_mapping
 
+
 class UtilTest(TestCase):
 
     def test_namedtuple_from_mapping_can_succeed(self):
         pass
+
 
 if __name__ == "__main__":
     main()
