7196510b51aefb8c5cb4788b50187f464ca8f0bb
james7132 <contact@jamessliu.com>
2017-05-20 17:59:04 -0700

    Add placeholder test

---
+++ test/test_util.py
@@ -0,0 +1,10 @@
+from unittest import TestCase, main
+from util import namedtuple_from_mapping
+
+class UtilTest(TestCase):
+
+    def test_namedtuple_from_mapping_can_succeed(self):
+        pass
+
+if __name__ == "__main__":
+    main()
