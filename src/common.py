d02769c5bad26b689bdafd97f623f1e59a9b7ade
james7132 <james.siqi.liu@gmail.com>
2017-05-30 21:48:05 -0700

    Add file logging

--- src/common.py
+++ src/common.py
@@ -1,8 +1,13 @@
 import asyncio
 import sys
+import logging
 from quamash import QEventLoop
 from config import TRANSLATIONS
 from PyQt5.QtWidgets import QApplication
+
+logging.basicConfig(filename='launcher_log.txt',
+                    filemode='w',
+                    level=logging.INFO)
 
 app = QApplication(sys.argv)
 loop = QEventLoop(app)
