19c7fdd3a41aa748903e5003e0c4a4ead6fae82a
james7132 <james.siqi.liu@gmail.com>
2017-05-30 20:14:10 -0700

    Add Icons to GUI

--- src/main.py
+++ src/main.py
@@ -1,6 +1,23 @@
-from config import CONFIG
+import os
+from PyQt5 import QtGui, QtCore
+from config import CONFIG, RESOURCE_DIR
 from ui import MainWindow
-from common import loop
+from common import app, loop
+
+
+app_icon = QtGui.QIcon()
+app_icon.addFile(os.path.join(RESOURCE_DIR, 'img/16x16.ico'),
+                 QtCore.QSize(16, 16))
+app_icon.addFile(os.path.join(RESOURCE_DIR, 'img/24x24.ico'),
+                 QtCore.QSize(24, 24))
+app_icon.addFile(os.path.join(RESOURCE_DIR, 'img/32x32.ico'),
+                 QtCore.QSize(32, 32))
+app_icon.addFile(os.path.join(RESOURCE_DIR, 'img/48x48.ico'),
+                 QtCore.QSize(48, 48))
+app_icon.addFile(os.path.join(RESOURCE_DIR, 'img/app.ico'),
+                 QtCore.QSize(256, 256))
+app.setWindowIcon(app_icon)
+
 
 main_window = MainWindow(CONFIG)
 
