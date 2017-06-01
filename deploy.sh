732df21c7cd6dfe9794ce2c8fd4724d5fcacde27
james7132 <contact@jamessliu.com>
2017-06-01 01:05:46 -0700

    Add shorter keepalive timer

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@ ls -lah dist/*
 for file in $(ls dist/*)
 do
 curl -i -X POST $DEPLOY_UPLOAD_URL/$TRAVIS_BRANCH/$TRAVIS_OS_NAME?token=$TOKEN \
-        -T $file
+        -T $file \
+        --keepalive-time 2
 done
