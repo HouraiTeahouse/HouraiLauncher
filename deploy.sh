56723ef410b37ae150b0e9a003fc1b26f680e424
james7132 <james.siqi.liu@gmail.com>
2017-06-01 01:30:45 -0700

    Switch back to form upload

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 ls -lah dist/*
 for file in $(ls dist/*)
 do
-curl -ivs -X POST $DEPLOY_UPLOAD_URL/$TRAVIS_BRANCH/$TRAVIS_OS_NAME?token=$TOKEN \
-        -T $file \
-        --keepalive-time 2 > /dev/null
+curl -i -X POST $DEPLOY_UPLOAD_URL/$TRAVIS_BRANCH/$TRAVIS_OS_NAME?token=$TOKEN \
+        -F "file:@dist/$file" \
+        --keepalive-time 2
 done
