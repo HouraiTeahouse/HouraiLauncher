56e6e02407accab67ca3ad03fc6c106fde8f76f9
james7132 <contact@jamessliu.com>
2017-06-01 01:22:19 -0700

    Add diagnistic data to deployment

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 ls -lah dist/*
 for file in $(ls dist/*)
 do
-curl -i -X POST $DEPLOY_UPLOAD_URL/$TRAVIS_BRANCH/$TRAVIS_OS_NAME?token=$TOKEN \
+curl -ivs -X POST $DEPLOY_UPLOAD_URL/$TRAVIS_BRANCH/$TRAVIS_OS_NAME?token=$TOKEN \
         -T $file \
-        --keepalive-time 2
+        --keepalive-time 2 > /dev/null
 done
