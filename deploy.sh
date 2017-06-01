6ce87494c35379aaa645007714e36f8844a7f8d8
james7132 <contact@jamessliu.com>
2017-06-01 01:42:48 -0700

    Fix file reference

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ ls -lah dist/*
 for file in $(ls dist/*)
 do
 curl -i -X POST $DEPLOY_UPLOAD_URL/$TRAVIS_BRANCH/$TRAVIS_OS_NAME?token=$TOKEN \
-        -F "file=@dist/$file" \
+        -F "file=@$file" \
         --keepalive-time 2
 done
