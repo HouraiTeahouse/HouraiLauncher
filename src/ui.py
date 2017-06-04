554ccda5af74af6a34527885687593f6e721b875
james7132 <contact@jamessliu.com>
2017-06-04 14:33:22 -0700

    Log more about downloading

--- src/ui.py
+++ src/ui.py
@@ -74,8 +74,11 @@ class Download(object):
             logging.info('Response: %s (%s)' %
                          (response.status_code, self.url))
             for block in response.iter_content(CHUNK_SIZE):
+                logging.info('Downloaded chunk of (size: %s, %s)' % (len(block),
+                    path))
                 self.downloaded_bytes += len(block)
                 downloaded_file.write(block)
+        logging.info('Done downloading: %s' % path)
         return response.status_code
 
 
