7fb6fdf694e308f680716fb7d3705f74360a9188
James Liu <contact@jamessliu.com>
2017-06-13 00:04:53 -0700

    Only deploy on master

--- scripts/deploy_win.bat
+++ scripts/deploy_win.bat
@@ -1,8 +1,9 @@
+if %APPVEYOR_REPO_BRANCH% == "master" (
+        for /r %%i in (dist/*) do echo %%i
 
-for /r %%i in (dist/*) do echo %%i
-
-for %%f in (dist/*) do (
-curl.exe -i -X POST "%DEPLOY_UPLOAD_URL%/%APPVEYOR_REPO_BRANCH%/Windows?token=%TOKEN%" ^
-        -F "file=@dist/%%f" ^
-        --keepalive-time 2
+        for %%f in (dist/*) do (
+        curl.exe -i -X POST "%DEPLOY_UPLOAD_URL%/%APPVEYOR_REPO_BRANCH%/Windows?token=%TOKEN%" ^
+                -F "file=@dist/%%f" ^
+                --keepalive-time 2
+        )
 )
