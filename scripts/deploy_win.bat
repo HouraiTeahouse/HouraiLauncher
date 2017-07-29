83c1af7496858b1ba04117484f230564414bb59d
james7132 <james.siqi.liu@gmail.com>
2017-07-29 00:51:12 -0700

    Attempt to fix Windows deployment

--- scripts/deploy_win.bat
+++ scripts/deploy_win.bat
@@ -1,11 +1,14 @@
 echo "Branch: %APPVEYOR_REPO_BRANCH%"
 echo "Pull Request: %APPVEYOR_PULL_REQUEST_NUMBER%"
 if %APPVEYOR_REPO_BRANCH% == "master" (
+        echo "Starting Deployment..."
         for /r %%i in (dist/*) do echo %%i
 
         for %%f in (dist/*) do (
+        echo "Deploying %%f"
         curl.exe -i -X POST "%DEPLOY_UPLOAD_URL%/%APPVEYOR_REPO_BRANCH%/Windows?token=%TOKEN%" ^
                 -F "file=@dist/%%f" ^
                 --keepalive-time 2
         )
+        echo "Finished deployment..."
 )
